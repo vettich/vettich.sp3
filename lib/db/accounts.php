f19410d74116a9c73374a94321d00eeaa1f2ad4e
Vetti <vetti.ch@ya.ru>
2020-02-14 13:08:17 +0500

    added getListType, getById, getByIds methods

--- lib/db/accounts.php
+++ lib/db/accounts.php
@@ -5,20 +5,77 @@ use vettich\sp3\Module;
 
 class Accounts extends \vettich\devform\data\ArrayList
 {
+	private static $_accs = [];
+
 	public function __construct($args = [])
 	{
 		parent::__construct($args);
+		if (isset($args['filter'])) {
+			$this->filter = $args['filter'];
+		}
 	}
 
 	public function getList()
 	{
-		$res = Module::api()->accountsList();
+		$res = Module::api()->accountsList($this->filter);
 		if (!empty($res['error'])) {
 			return [];
 		}
 		$res = Module::convertToSiteCharset($res);
 		$accounts = $res['response']['accounts'];
+		self::$_accs = $accounts;
 		return $accounts;
+	}
+
+	public function getListType()
+	{
+		$res = $this->getList();
+		$list = [];
+		foreach ($res as $acc) {
+			if (empty($list[$acc['type']])) {
+				$list[$acc['type']] = [];
+			}
+			$list[$acc['type']][] = $acc;
+		}
+		return $list;
+	}
+
+	public static function getById($id)
+	{
+		if (empty($id)) {
+			return [];
+		}
+		if (isset(self::$_accs[$id])) {
+			return self::$_accs[$id];
+		}
+		$r = Module::api()->getAccount($id);
+		if (empty($r['error'])) {
+			$r = Module::convertToSiteCharset($r);
+			self::$_accs[$id] = $r['response'];
+			return $r['response'];
+		}
+		return [];
+	}
+
+	public static function getByIds($ids)
+	{
+		if (empty($ids)) {
+			return [];
+		}
+		$res = [];
+		foreach ($ids as $id) {
+			if (isset(self::$_accs[$id])) {
+				$res[$id] = self::$_accs[$id];
+				continue;
+			}
+			$r = Module::api()->getAccount($id);
+			if (empty($r['error'])) {
+				$r = Module::convertToSiteCharset($r);
+				$res[$id] = $r['response'];
+				self::$_accs[$id] = $r['response'];
+			}
+		}
+		return $res;
 	}
 
 	public function delete($name, $value)
