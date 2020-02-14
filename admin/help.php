88c49d5f8255f9d1a17586d403adb4c69fb80533
Vetti <vetti.ch@ya.ru>
2020-02-14 13:21:50 +0500

    added help page

--- admin/help.php
+++ admin/help.php
@@ -6,18 +6,23 @@ IncludeModuleLangFile(__FILE__);
 CModule::IncludeModule('vettich.sp3');
 use vettich\sp3\Module;
 
-$APPLICATION->SetTitle(Module::m('PAGE_TITLE'));
+$APPLICATION->SetTitle(Module::m('HELP_PAGE_TITLE'));
 
+\CJSCore::Init(['vettich_sp3_script']);
+echo Module::m('HELP_PAGE_CONTENT');
 ?>
-
-	<h2><?=Module::m('ABOUT_MODULE_TITLE')?></h2>
-	<div class="vsp3-content"><?=Module::m('ABOUT_MODULE')?></div>
-
-	<h2><?=Module::m('USER_TITLE')?></h2>
-	<div class="vsp3-content"><?=Module::m('USER')?></div>
-
-	<h3><?=Module::m('USER_BALANCE_TITLE')?></h3>
-	<div class="vsp3-content"><?=Module::m('USER_BALANCE')?></div>
-
+<style>
+	h4 {
+		color: #25282C;
+	}
+	.vettich-sp3-content {
+		font-size: 120%;
+	}
+	.vettich-sp3-img {
+		max-width: 100%;
+		box-shadow: 0px 0px 15px 0px gray;
+	}
+</style>
 <?php
+
 require_once($_SERVER["DOCUMENT_ROOT"].BX_ROOT."/modules/main/include/epilog_admin.php");
